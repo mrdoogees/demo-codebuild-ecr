7f8cc080fbe7a3fd8e32a2dddaba071c45a81fda
Randrianalivelo Manohisoa <manohisoa.randrianalivelo-cic@ibm.com>
2022-06-07 11:06:56 +0200

    Init commit

---
+++ src/main/java/com/cozla/democodebuildecr/DemoCodebuildEcrApplication.java
@@ -0,0 +1,13 @@
+package com.cozla.democodebuildecr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoCodebuildEcrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoCodebuildEcrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cozla/democodebuildecr/DemoCodebuildEcrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cozla.democodebuildecr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoCodebuildEcrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
